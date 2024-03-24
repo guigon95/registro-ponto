92749f13bcd079518a5a9f4ed6f860a8ae784d84
guigon95 <guilherme_gon@live.com>
2024-03-24 18:23:01 -0300

    server port 8080

--- src/test/java/com/hackathon/registroponto/RegistroPontoApplicationTests.java
+++ src/test/java/com/hackathon/registroponto/RegistroPontoApplicationTests.java
@@ -3,10 +3,9 @@ package com.hackathon.registroponto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class RegistroPontoApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
