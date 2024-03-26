b828bd9671dfd8accfca4ae08f92bbdcc03cd11c
Guilherme <guilherme_gon@live.com>
2024-03-25 22:35:15 -0300

    includindo novos registros

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,8 @@ insert into registro_ponto_entity values ('2024-02-22 09:20:07.365154', 'd9180ac
 insert into registro_ponto_entity values ('2024-02-22 12:20:07.365154', 'd9180acb-392b-4253-912b-4f299b11be95', uuid());
 insert into registro_ponto_entity values ('2024-02-22 13:20:07.365154', 'd9180acb-392b-4253-912b-4f299b11be95', uuid());
 insert into registro_ponto_entity values ('2024-02-22 18:20:07.365154', 'd9180acb-392b-4253-912b-4f299b11be95', uuid());
+
+insert into registro_ponto_entity values ('2024-02-21 09:20:07.365154', 'd9180acb-392b-4253-912b-4f299b11be95', uuid());
+insert into registro_ponto_entity values ('2024-02-21 12:20:07.365154', 'd9180acb-392b-4253-912b-4f299b11be95', uuid());
+insert into registro_ponto_entity values ('2024-02-21 13:20:07.365154', 'd9180acb-392b-4253-912b-4f299b11be95', uuid());
+insert into registro_ponto_entity values ('2024-02-21 18:20:07.365154', 'd9180acb-392b-4253-912b-4f299b11be95', uuid());
